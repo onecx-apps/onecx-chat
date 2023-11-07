e56384b17fe27c5e830ed0a2afd65ecb28ac7e04
Michael Gloeckner <michael.gloeckner@capgemini.com>
2023-11-07 15:31:32 +0100

    updated to new cluster installation

--- helm/install.sh
+++ helm/install.sh
@@ -5,12 +5,20 @@ aws ec2 create-volume --availability-zone us-east-1a --size 24
 aws ec2 create-volume --availability-zone us-east-1a --size 10
 
 
+#take volume id and set it in pv.yaml files of llama2 and qdrant
+
 #install storage-class
 kubectl apply -f storageclass.yaml
 
 #install llama2
-helm install llama2 -n genai ./llama2
+helm install llama2 -n genai --create-namespace ./llama2
 
 #install qdrant
-helm install qdrant -n genai ./qdrant
+helm install qdrant -n genai --create-namespace ./qdrant
 
+#update route 53 to loadbalancers 
+
+#check connection
+http://llama2.one-cx.org:8000/
+
+http://qdrant.one-cx.org:6333/
